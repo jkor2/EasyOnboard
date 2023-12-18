c84a72c2b70cadd2dc79bfe0ee8fce395a0998fe
Jake Korobellis <jkorobellis@gmail.com>
2023-12-18 01:28:08 -0500

    Backend Skelton

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const connectDB = require('./config/db')
+const app = express()
+
+// Connect to DB 
+connectDB()
+
+
+app.use(express.json({ extended: false }))
+
+
+
+
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log("Server running on ", PORT)
+})
