dddae1f50f95920b245f8fd13a5a0d2962e32fdc
Jake Korobellis <jkorobellis@gmail.com>
2023-12-20 22:57:32 -0500

    Handling the new upload of an employee

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,40 +1,55 @@
-const express = require("express")
-const router = express.Router()
-const User = require("../models/User")
-
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 
 // @route   POST /api/employee
-// @desc    Create new user 
+// @desc    Create new user
 
-router.post("/newuser", async (req, res) => {
-    const user =  req.body
-    console.log(user)
+router.post("/api/users/individual", async (req, res) => {
+  const employeeExisist = User.findOne({ email: req.body.email });
 
-    // Clean user data entries 
+  const user = req.body;
+  console.log(user);
 
+  try {
+    if (!employeeExisist) {
+      // If new employee is not in database
 
-    const nuser = new User({
-    fname: req.body.fname,
-    lname: req.body.lname,
-    email: req.body.email,
-    phone_number: req.body.phone_number,
-    position: req.body.position,
-    location: req.body.location, 
-     })
-     
-     await nuser.save()
-     res.status(200)
-     
-    
+      // Clean user data entries
+      const nuser = new User({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        phone_number: req.body.phone_number,
+        position: req.body.position,
+        location: req.body.location,
+      });
 
-})
+      await nuser.save();
+      res.json({
+        status: 200,
+        response: `Success! ${req.body.fname} has been added.`,
+      });
+    } else {
+      // Response if employee is in DB
+      res.json({
+        status: 200,
+        response: `${req.body.fname} is already in the database!`,
+      });
+    }
+  } catch (err) {
+    // Catch a server error
+    res.json({
+      status: 500,
+      response: `Uh Oh! Server error trying to upload ${req.body.fname}.`,
+    });
+  }
+});
 
-router.get("/users", async(req, res) => {
-    data = await User.find({})
-    console.log(data)
-    res.json({data: data})
-})
+router.get("/users", async (req, res) => {
+  data = await User.find({});
+  console.log(data);
+  res.json({ data: data });
+});
 
-
-
-module.exports = router
+module.exports = router;
